d894b754f317286482649c6e12d0b7a9cfaccd95
Daniel Callus <danielcallus@gmail.com>
2021-05-06 08:34:04 +0100

    tried to break down iso, still a bit lost

--- anagram_finder/models/anagram_finder.js
+++ anagram_finder/models/anagram_finder.js
@@ -1,11 +1,11 @@
 const AnagramFinder = function (word) {
-    this.word = word
-    this.sortedWord = word.toLowerCase().split('').sort().join('')
+    this.word = word;
+    this.sortedWord = word.toLowerCase().split('').sort().join('');
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
     // for word in other wordsList. if word.sorted == this.word.sorted
-    return otherWords.filter(word => word.toLowerCase().split('').sort().join('') === this.sortedWord && word !== this.word)
+    return otherWords.filter(word => word.toLowerCase().split('').sort().join('') === this.sortedWord && word !== this.word);
 }
 
 module.exports = AnagramFinder;
